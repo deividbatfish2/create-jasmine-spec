3c4ecc6a64a0b731fffadffb353888baa33a2f97
Deivid Teixeira <deividbatfish2@gmail.com>
2018-06-11 18:36:18 -0300

    Gera arquivos .js a partir dos arquivos .features

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 const reader = require('./lib/reader')
 const file = require('./lib/files')
+const specs = require('./lib/specs')
+const template = require('./lib/templates/spec.template')
 
 const filePath = process.argv[2] || './playground'
 
 const run = async () => {
+    let contexto
+    let arrayCenarios = []
     let featuresFileList = await file.recuperarListaFeatures(filePath)
     featuresFileList.forEach(arquivo => {
-        const arrayDeCenarios = reader.extrairCenarios(arquivo)
-        console.log(arrayDeCenarios)
+        const arrayDeFuncoes = reader.extrairCenarios(arquivo)
+        arrayDeFuncoes.forEach(cenario => {
+            if (specs.isCenario(cenario)) {
+                arrayCenarios.push(template.criarCenario(cenario.funcao, cenario.passos))
+            }
+
+            if (specs.isContexto(cenario)) {
+                contexto = template.criarContexto(cenario.passos)
+            }
+        })
+
+        arrayCenarios.forEach((cenario, index) => {
+            let template = `
+${contexto || ''}
+${cenario}`
+            file.criarArquivoFeature(arquivo, template, index)
+        })
+        arrayCenarios = []
     });
 
 }
